a514ce4c6c8da9439f69f0b57407f53d9a250d97
zc <zc-yun@outlook.com>
2023-11-21 22:00:36 +0800

    1

--- plugins/zp116_拖拽排序.js
+++ plugins/zp116_拖拽排序.js
@@ -1,6 +1,6 @@
-function init({ ctx, exc, props, container }) {
+function init({ ctx, exc, excA, props, container }) {
     exc('load("https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js")', {}, () => {
-        let arr = exc(props.arr, ctx)
+        let arr = excA(props.arr, ctx)
         if (!Array.isArray(arr)) return warn("未配置待排序数组")
         let el = props.selector ? $(props.selector) : container.previousElementSibling
         if (!el) return log("未找到目标容器")
